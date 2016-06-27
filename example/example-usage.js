a6ed81bce0fc1a54e1b02ee5587500a97c9826f9
Taka Okunishi <okunishitaka.com@gmail.com>
2016-06-27 15:58:37 +0900

    Version incremented to 1.0.1

--- example/example-usage.js
+++ example/example-usage.js
@@ -4,11 +4,22 @@ const sugoAgentCompile = require('sugo-agent-compile')
 const co = require('co')
 
 co(function * () {
-  let agent = sugoAgentCompile('http://example.com')
+  let agent = sugoAgentCompile('http://my-server.com')
 
+  // Check if server available
   {
-    // Check if server available
-    let ok = yield agent.knock()
+    let ok = yield agent.knock() // Send HTTP HEAD request.
+    /** ... */
+  }
+
+  // Compile es6 script
+  {
+    let myScript = `
+let foo = (...arg) => ['foo', ...args].join('')
+foo()
+`
+    let compiled = yield agent.compile(myScript)
+    console.log(compiled)
     /** ... */
   }
 }).catch((err) => console.error(err))
